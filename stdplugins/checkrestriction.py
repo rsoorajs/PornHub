12e463ec69294e974e914369a0beb77d3448674e
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2020-09-03 23:24:31 +0530

    remove @CheckRestrictionsBot, since we are already running as an user -_-

--- stdplugins/checkrestriction.py
+++ stdplugins/checkrestriction.py
@@ -1,8 +1,9 @@
-
-import datetime
-from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
+# modified by @UniBorg
+from telethon.tl.types import (
+    Channel,
+    Chat,
+    User
+)
 from uniborg.util import admin_cmd
 
 @borg.on(admin_cmd(pattern="res ?(.*)"))
@@ -13,22 +14,35 @@ async def _(event):
        await event.edit("```Reply to a Link.```")
        return
     reply_message = await event.get_reply_message() 
-    if not reply_message.text:
+    if not reply_message.entities:
        await event.edit("```Reply to a Link```")
        return
-    chat = "@CheckRestrictionsBot"
-    sender = reply_message.sender
-    await event.edit("```Processing```")
-    async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=894227130))
-              await event.client.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("`RIP Check Your Blacklist Boss`")
-              return
-          if response.text.startswith(""):
-             await event.edit("Am I Dumb Or Am I Dumb?")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+    # chat = "@CheckRestrictionsBot"
+    reply_entity = reply_message.entities[0]
+    reply_res = reply_message.text[reply_entity.offset:reply_entity.offset+reply_entity.length]
+    rr = await event.client.get_entity(reply_res)
+    await event.edit(get_restriction_string(rr))
+
+
+def get_restriction_string(a) -> str:
+    b = ""
+    c = ""
+    if isinstance(a, Channel):
+        c = f"[{a.title}](https://t.me/c/{a.id}/{2})"
+    elif isinstance(a, User):
+        c = f"[{a.first_name}](tg://user?id={a.id})"
+    elif isinstance(a, Chat):
+        c = f"{a.title}"
+        b = f"{c}: basic groups do not have restriction, to the best of my knowledge"
+        return b
+    else:
+        c = "__UN-KNOWN__"
+    if a.restriction_reason is None or len(a.restriction_reason) == 0:
+        b = "{}: Good News! No Limitations are currently applied to this @CheckRestrictionsBot".format(c)
+        # plox: do not remove -_- this credit. thx.!
+    else:
+        tmp_string = f"{c} has the following restriction_reason(s): \n"
+        for a_r in a.restriction_reason:
+            tmp_string += f"👉 {a_r.reason}-{a_r.platform}: {a_r.text}\n\n"
+        b = tmp_string
+    return b
